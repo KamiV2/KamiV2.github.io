89da24f147413171726db58f93ef384b4caf487a
Kami-V2 <78351514+Kami-V2@users.noreply.github.com>
2022-07-18 17:05:16 +0530

    Updates

---
+++ precache-manifest.3da592edd7b2f5222061d2b05fd29a99.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef3c621f89d9d9be6cd4",
+    "url": "/static/css/main.77dcfba2.chunk.css"
+  },
+  {
+    "revision": "ef3c621f89d9d9be6cd4",
+    "url": "/static/js/main.ef3c621f.chunk.js"
+  },
+  {
+    "revision": "9e9f3b6596b09c802a9d",
+    "url": "/static/js/1.9e9f3b65.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6220ea8e735540545a0e2b1ee49e437",
+    "url": "/static/media/ai-ml.f6220ea8.png"
+  },
+  {
+    "revision": "f3ad2c78e062a6a591135a34c38d2c55",
+    "url": "/static/media/Karun-Ram-Resume.f3ad2c78.pdf"
+  },
+  {
+    "revision": "fcdbc1298d7f265a4e9dc85302a4d259",
+    "url": "/static/media/pfp.fcdbc129.jpg"
+  },
+  {
+    "revision": "aa2335b339562bfe68582c34caaf86d9",
+    "url": "/static/media/algo.aa2335b3.png"
+  },
+  {
+    "revision": "2f82f71ec82c940d64ef0ef863e92a51",
+    "url": "/static/media/web.2f82f71e.png"
+  },
+  {
+    "revision": "fda2c40f870348a75c2e1c6458d5a46f",
+    "url": "/static/media/fun.fda2c40f.png"
+  },
+  {
+    "revision": "dbcd9aac27d333ec6d35e0a108dfb50d",
+    "url": "/index.html"
+  }
+];
